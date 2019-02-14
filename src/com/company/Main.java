52e78ec7df1437515c89e7f87dbd68fcf3c8cdb9
mariya <mariyascott136@gmail.com>
2019-02-14 00:37:40 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int intValue = 93;
+        int intValue2 = 56;
+
+        int result1 = intValue + intValue2;
+        System.out.println(result1);
+
+        int result2 = intValue - intValue2;
+        System.out.println(result2);
+
+        int result3 = intValue * intValue2;
+        System.out.println(result3);
+
+        int result4 = intValue / intValue2;
+        System.out.println(result4);
+
+        int result5 = intValue % intValue2;
+        System.out.println(result5);
+    }
+}
